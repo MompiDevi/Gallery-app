ece5f3c40a1968d056abbb3136b520ddda0d5323
MompiDevi <mompi.momiv@gmail.com>
2018-12-27 22:50:40 +0530

    Create CustomCollectionViewCell.swift

---
+++ Gallery App/GalleryApp/CustomCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.swift
+//  GalleryApp
+//
+//  Created by N. Mompi Devi on 28/06/18.
+//  Copyright © 2018 momiv. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImageView: UIImageView!
+}
