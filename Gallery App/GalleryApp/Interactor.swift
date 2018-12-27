5129fa6dfb8029ea5505a4202a5cb586ee069fbf
MompiDevi <mompi.momiv@gmail.com>
2018-12-27 22:50:56 +0530

    Create Interactor.swift

---
+++ Gallery App/GalleryApp/Interactor.swift
@@ -0,0 +1,13 @@
+//
+//  Interactor.swift
+//  GalleryApp
+//
+//  Created by N. Mompi Devi on 05/08/18.
+//  Copyright © 2018 momiv. All rights reserved.
+//
+
+import UIKit
+class Interactor: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+}
